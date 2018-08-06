2c54def742918532e84c459eff641f9751852bb5
Herbert Weigert Venancio <hwvenancio@gmail.com>
2018-08-06 09:52:34 -0300

    added 'extern "C"' to export functions

--- Vec3.cpp
+++ Vec3.cpp
@@ -8,15 +8,15 @@ public:
 
 POLYGLOT_DECLARE_TYPE(btVector3)
 
-void *allocNativeVector() {
+extern "C" void *allocNativeVector() {
     btVector3 *ret = new btVector3();
     return polyglot_from_btVector3(ret);
 }
 
-void freeNativeVector(btVector3 *p) {
+extern "C" void freeNativeVector(btVector3 *p) {
     delete p;
 }
 
-void printVector(btVector3 *p) {
+extern "C" void printVector(btVector3 *p) {
     printf("btVector3<%f,%f,%f>\n", p->m_floats[0], p->m_floats[1], p->m_floats[2]);
 }
